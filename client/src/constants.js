9c5f18a654b1896b245a55f90b17772f6a8fc532
ferdeolazabal <ferdeolazabal@gmail.com>
2022-02-02 21:53:42 -0300

    constants variable added

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,8 +2,8 @@ const { REACT_APP_HOST } = process.env;
 
 const constants = {
 
-    POKEMONS_URL: `${REACT_APP_HOST}/pokemons`,
-    TYPES_URL: `${REACT_APP_HOST}/types`,
+    POKEMONS_URL: `${REACT_APP_HOST || "http://localhost:3001"}/pokemons`,
+    TYPES_URL: `${REACT_APP_HOST || "http://localhost:3001"}/types`,
 
 };
 
